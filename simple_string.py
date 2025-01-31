75728b209c5b67101f0d1c15a19390c08adb41c5
Andre de Leon <andredeleon@Andres-MacBook-Pro-2.local>
2025-01-31 13:20:41 +0100

    Initial commit

---
+++ simple_string.py
@@ -0,0 +1,11 @@
+s = "Hello"
+print(s)
+s2 = "Hello sir @#$%^&*@!"
+print(s2)
+s = ('And she said: "Hello!"')
+print(s)
+print(",.'\"\"\"")
+s = "hello!"
+print(s[2], s[4]) # index by postion, first index is zero
+print(s[-1]) #go around to the ends!
+print(s[1], s[-5])
---
+++ string_methods.py
@@ -0,0 +1,48 @@
+from idlelib.replace import replace
+
+print(dir("hello"))
+
+print(help("hi".capitalize()))
+print("i like to go to school".capitalize())
+print("IS THIS SUPPOSED TO WORK?".capitalize())
+print("hello".center(50, "x"))
+print("gmail.com".find("."))
+print("gmail.com".find("john"))
+s = "i see a cat who like to cat while i cat on a cat"
+#find all occurrences
+poz = 0
+while True:
+    poz = s.find("cat", poz)
+    if poz == -1:
+        break
+    print("found cat on position", poz)
+    poz += 1
+
+
+#join
+
+#lower
+s = "I SEE A LOT OF THINGS!"
+print(s.lower())
+
+#upper
+s = "i see a lot of things"
+print(s.upper().capitalize())
+
+#replace - replaces X with Y
+s = "i see a cat who likes to eat a rat. what a nice cat"
+print(s.replace("c", "r"))
+print(s.replace("cat", "lion"))
+s = "Hello, kind sir! How are you today?"
+print(s.replace(",","").replace("!", "").replace("?",""))
+
+#split
+s = "I like to go shopping"
+print(s.split())
+splitted = s.split()
+print("XX".join(splitted))
+
+
+#title
+s = "i like the mountians"
+print(s.title())
